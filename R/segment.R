27aeff2820424d7304d24be20c9d95e45ae013a5
Brooke Anderson <brooke.anderson@colostate.edu>
2017-01-30 22:13:24 -0700

    Update documentation and code style for `segment`

--- R/segment.R
+++ R/segment.R
@@ -1,7 +1,4 @@
-#' @name segment
-#' @aliases segment
-#' @title segment
-#' @description segment
+#' segment
 #'
 #' @param Y \eqn{p x n} data matrix, with \eqn{p} time series of length \eqn{n}
 #' @param mean_y mean vector
@@ -11,23 +8,26 @@
 #'
 #' @return b An optimal bandwidth.
 #'
-#' @references J. Chang, Q. Yao, and W. Zhou (2016) Testing for high-dimensional white noise using maximum cross correlations. \emph{Biometrika}, to appear.
-#' @author Meng Cao
-#' @export
+#' @references
+#' J. Chang, Q. Yao, and W. Zhou (2016) Testing for high-dimensional white noise using
+#' maximum cross correlations. \emph{Biometrika}, to appear.
 #'
-segment = function(Y, mean_y, k, n, p){
-  b = rep(0, p^2)
-  for (t in (1:(n-k))){
-    s = 0
-    C = Y[,(t+k)] - mean_y
-    D = Y[, t] - mean_y
+#' @author Meng Cao \email{meng.cao@colostate.edu}
+#'
+#' @export
+segment <- function(Y, mean_y, k, n, p){
+  b <- rep(0, p ^ 2)
+  for (t in (1:(n - k))){
+    s <- 0
+    C <- Y[ , (t + k)] - mean_y
+    D <- Y[ , t] - mean_y
     for (i in (1:p)){
       for (j in (1:p)){
-        s = s+1
-        b[s] = b[s] + C[i]*D[j]
+        s <- s + 1
+        b[s] <- b[s] + C[i] * D[j]
       }
     }
   }
-  b = b/n
+  b <- b / n
   return (b)
 }
